8cd3857b9685af58d2c6bb09164e510a089fa2c9
MKSIAW <michellekyere@gmail.com>
2024-01-06 16:35:23 -0500

    VirtualTour

---
+++ src/VirtualTour.js
@@ -0,0 +1,13 @@
+// VirtualTour.js
+import React from 'react';
+
+const VirtualTour = () => {
+  return (
+    <div>
+      <h1>Virtual Tour</h1>
+      {/* Add content for the virtual tour */}
+    </div>
+  );
+};
+
+export default VirtualTour;
