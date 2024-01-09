408fb353ada70f2f9b82b234f2838bb2fe3a2a71
MKSIAW <michellekyere@gmail.com>
2024-01-09 14:09:49 -0500

    Update singin

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,10 +1,33 @@
+// SignIn.js
 import React from 'react';
 
 const SignIn = () => {
   return (
-    <div className="sign-in-page">
+    <div className="sign-in-container">
       <h2>Sign In</h2>
-      {/* Add your sign-in form components here */}
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" />
+
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+
+        <div className="additional-options">
+          <div>
+            <input type="checkbox" id="rememberMe" name="rememberMe" />
+            <label htmlFor="rememberMe">Remember me</label>
+          </div>
+          <p>
+            <a href="/forgot-password">Forgot password?</a>
+          </p>
+        </div>
+
+        <button type="submit">Sign In</button>
+      </form>
+
+      <p>
+        Don't have an account? <a href="/sign-up">Sign Up</a>
+      </p>
     </div>
   );
 };
